a1617a0a73979d493e9ef31b1619f16163a711f4
samuelkaranja <samuelkaranja007@gmail.com>
2023-10-18 18:09:10 +0300

    Work Component Update

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,53 +1,56 @@
-import Work1 from '../../assets/work1.jpg';
-import Work2 from '../../assets/work2.jpg';
-import Work3 from '../../assets/work3.jpg';
-import Work4 from '../../assets/work4.jpg';
-import Work5 from '../../assets/work5.jpg';
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
-    {
-      id: 1,
-      image: Work1,
-      title: "Web design",
-      category: "web",
-    },
-    {
-      id: 2,
-      image: Work2,
-      title: "App movil",
-      category: "app",
-    },
-    {
-      id: 3,
-      image: Work3,
-      title: "Brand design",
-      category: "design",
-    },
-    {
-      id: 4,
-      image: Work4,
-      title: "App movil",
-      category: "app",
-    },
-    {
-      id: 5,
-      image: Work5,
-      title: "Web design",
-      category: "web",
-    },
+  {
+    id: 1,
+    image: Work1,
+    url: "https://samuel-tic-tac-toe.netlify.app/",
+    title: "Tic-Tac-Toe",
+    category: "web",
+  },
+  {
+    id: 2,
+    image: Work2,
+    url: "https://sams-ecommerce-website.netlify.app/",
+    title: "E-Commerce",
+    category: "app",
+  },
+  {
+    id: 3,
+    image: Work3,
+    url: "https://samuelbestsellers.netlify.app/",
+    title: "Book Sales",
+    category: "design",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Web design",
+    category: "web",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: 'all',
-    },
-    {
-        name: 'web',
-    },
-    {
-        name: 'app',
-    },
-    {
-        name: 'design',
-    },
-]
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+  {
+    name: "design",
+  },
+];
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 const WorkItems = ({ item }) => {
-    return (
-        <div className="work__card" key={item.id}>
-            <img src={item.image} alt="" className="work__img" />
-            <h3 className="work__title">{item.title}</h3>
-            <a href="/#" target="_blank" className="work__button">
-                Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="work__card" key={item.id}>
+      <img src={item.image} alt="" className="work__img" />
+      <h3 className="work__title">{item.title}</h3>
+      <a
+        href={item.url}
+        target="_blank"
+        rel="noreferrer"
+        className="work__button"
+      >
+        Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+      </a>
+    </div>
+  );
+};
 
 export default WorkItems;
