d2408c06ffec11b5f7b60ffa39de10a271581c85
samuelkaranja <samuelkaranja007@gmail.com>
2023-10-23 15:55:25 +0300

    Updated Work data

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,8 @@
-import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
 import tictactoe from "../../assets/tictactoe.png";
 import books from "../../assets/books.jpg";
 import ecommerce from "../../assets/ecommerce.jpg";
+import recipe from "../../assets/recipe.jpg";
 
 export const projectsData = [
   {
@@ -28,9 +28,10 @@ export const projectsData = [
   },
   {
     id: 4,
-    image: Work4,
-    title: "App movil",
-    category: "app",
+    image: recipe,
+    url: "https://sams-recipe-app.netlify.app/",
+    title: "Recipe",
+    category: "web",
   },
   {
     id: 5,
