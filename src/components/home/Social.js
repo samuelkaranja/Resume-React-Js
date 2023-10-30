50e4d86d14e2359aec9e0a1dfc31b2133d161c2f
samuelkaranja <samuelkaranja007@gmail.com>
2023-10-30 20:40:09 +0300

    added article links

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Social = () => {
-	return (
-		<div className="home__social">
-			<a href="https://www.instagram.com/" className="home__social-icon" target="_blank" rel="noreferrer"><i class="uil uil-instagram"></i></a>
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.baeldung.com/linux/author/samuelnjugunakaranja/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-pen"></i>
+      </a>
 
-			<a href="https://www.linkedin.com/in/samuel-karanja-207112181/" className="home__social-icon" target="_blank" rel="noreferrer"><i class="uil uil-linkedin"></i></a>
+      <a
+        href="https://www.linkedin.com/in/samuel-karanja-207112181/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
 
-			<a href="https://github.com/samuelkaranja" className="home__social-icon" target="_blank" rel="noreferrer"><i class="uil uil-github-alt"></i></a>
-		</div>
-	)
-}
+      <a
+        href="https://github.com/samuelkaranja"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
 
 export default Social;
