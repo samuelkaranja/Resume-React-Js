f1f53b7c58989f563803688b049ed052f958fb1e
samuelkaranja <samuelkaranja007@gmail.com>
2023-12-16 01:07:50 +0300

    Added football prediction link

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,44 +1,52 @@
-import Work5 from "../../assets/work5.jpg";
+// import Work5 from "../../assets/work5.jpg";
 import tictactoe from "../../assets/tictactoe.png";
 import books from "../../assets/books.jpg";
 import ecommerce from "../../assets/ecommerce.jpg";
 import recipe from "../../assets/recipe.jpg";
+import ball from "../../assets/ball.jpg";
 
 export const projectsData = [
   {
     id: 1,
+    image: ball,
+    url: "https://oasistips.com/",
+    title: "Football Predictions",
+    category: "web",
+  },
+  {
+    id: 2,
     image: tictactoe,
     url: "https://samuel-tic-tac-toe.netlify.app/",
     title: "Tic-Tac-Toe",
     category: "web",
   },
   {
-    id: 2,
+    id: 3,
     image: ecommerce,
     url: "https://sams-ecommerce-website.netlify.app/",
     title: "E-Commerce",
     category: "web",
   },
   {
-    id: 3,
+    id: 4,
     image: books,
     url: "https://samuelbestsellers.netlify.app/",
     title: "Book Sales",
     category: "web",
   },
   {
-    id: 4,
+    id: 5,
     image: recipe,
     url: "https://sams-recipe-app.netlify.app/",
     title: "Recipe",
     category: "web",
   },
-  {
-    id: 5,
-    image: Work5,
-    title: "Web design",
-    category: "web",
-  },
+  // {
+  //   id: 5,
+  //   image: Work5,
+  //   title: "Web design",
+  //   category: "web",
+  // },
 ];
 
 export const projectsNav = [
