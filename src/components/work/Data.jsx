ec3546882101ef8736054380285d8a1c7f30ae11
samuelkaranja <samuelkaranja007@gmail.com>
2025-07-04 16:41:07 +0300

    Added a project in the work/portfolio section

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,40 +1,48 @@
 import tictactoe from "../../assets/tictactoe.png";
 import ecommerce from "../../assets/ecommerce.jpg";
-import recipe from "../../assets/recipe.jpg";
+//import recipe from "../../assets/recipe.jpg";
 import blog from "../../assets/blog.jpg";
 import notes from "../../assets/notes.jpg";
+import quiz from "../../assets/quiz.jpg";
 
 export const projectsData = [
   {
     id: 1,
+    image: quiz,
+    url: "https://samsmindsprint.netlify.app/",
+    title: "Quiz App",
+    category: "web",
+  },
+  {
+    id: 2,
     image: notes,
     url: "https://my-notevault.netlify.app/",
     title: "Notes Vault",
     category: "web",
   },
   {
-    id: 2,
+    id: 3,
     image: blog,
     url: "https://penned.netlify.app/",
     title: "Penned",
     category: "web",
   },
+  // {
+  //   id: 4,
+  //   image: recipe,
+  //   url: "https://sams-recipe-app.netlify.app/",
+  //   title: "Recipe",
+  //   category: "web",
+  // },
   {
-    id: 3,
-    image: recipe,
-    url: "https://sams-recipe-app.netlify.app/",
-    title: "Recipe",
-    category: "web",
-  },
-  {
-    id: 4,
+    id: 5,
     image: ecommerce,
     url: "https://sams-ecommerce-website.netlify.app/",
     title: "E-Commerce",
     category: "web",
   },
   {
-    id: 5,
+    id: 6,
     image: tictactoe,
     url: "https://samuel-tic-tac-toe.netlify.app/",
     title: "Tic-Tac-Toe",
