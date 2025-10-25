0c67616aab64a8dbeff6db707861e795b088ab28
samuelkaranja <samuelkaranja007@gmail.com>
2025-10-25 17:04:29 +0300

    Added another project in the work section

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,11 +1,8 @@
 import personal from "../../assets/personal.jpg";
-//import tictactoe from "../../assets/tictactoe.png";
-//import ecommerce from "../../assets/ecommerce.jpg";
-//import recipe from "../../assets/recipe.jpg";
-//import blog from "../../assets/blog.jpg";
 import notes from "../../assets/notes.jpg";
 import quiz from "../../assets/quiz.jpg";
 import photo from "../../assets/photo.jpg";
+import market from "../../assets/market.jpg";
 
 export const projectsData = [
   {
@@ -24,46 +21,25 @@ export const projectsData = [
   },
   {
     id: 3,
+    image: market,
+    url: "https://kemonkmarketplace.netlify.app/",
+    title: "MarketPlace",
+    category: "web",
+  },
+  {
+    id: 4,
     image: quiz,
     url: "https://samsmindsprint.netlify.app/",
     title: "Quiz App",
     category: "web",
   },
   {
-    id: 4,
+    id: 5,
     image: notes,
     url: "https://my-notevault.netlify.app/",
     title: "Notes Vault",
     category: "web",
   },
-  // {
-  //   id: 3,
-  //   image: blog,
-  //   url: "https://penned.netlify.app/",
-  //   title: "Penned",
-  //   category: "web",
-  // },
-  // {
-  //   id: 4,
-  //   image: recipe,
-  //   url: "https://sams-recipe-app.netlify.app/",
-  //   title: "Recipe",
-  //   category: "web",
-  // },
-  // {
-  //   id: 5,
-  //   image: ecommerce,
-  //   url: "https://sams-ecommerce-website.netlify.app/",
-  //   title: "E-Commerce",
-  //   category: "web",
-  // },
-  // {
-  //   id: 6,
-  //   image: tictactoe,
-  //   url: "https://samuel-tic-tac-toe.netlify.app/",
-  //   title: "Tic-Tac-Toe",
-  //   category: "web",
-  // },
 ];
 
 export const projectsNav = [
